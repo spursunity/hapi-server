6eb2e646540601b1633e97c8746e9ad75ad94899
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-09-12 10:16:22 +0300

    add route for getting number

--- routes/message.js
+++ routes/message.js
@@ -14,11 +14,40 @@ const sendSimpleMessage = {
   options: {
     response: {
       status: {
-        200: Joi.string(),
+        200: Joi.object(),
+      },
+    },
+  },
+};
+
+const getNumber = {
+  method: 'GET',
+  path: '/num',
+  handler: async (request, h) => {
+    try {
+      const number = request.query.num;
+      const type = typeof number;
+
+      console.log(typeof number);
+
+      return h.response({ number, type }).code(200);
+    } catch(err) {
+      console.log('error route /num: ', err.message);
+    }
+  },
+  options: {
+    validate: {
+      query: {
+        num: Joi.number().integer().default(32),
+      },
+    },
+    response: {
+      status: {
+        200: Joi.object(),
       },
     },
   },
 };
 
 
-module.exports = [ sendSimpleMessage ];
+module.exports = [ sendSimpleMessage, getNumber ];
