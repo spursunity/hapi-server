e40a499e708eeb491ff88c3187f87cd5f88b7d49
Vitali Akulovich <vitali.akulovich.firefly@gmail.com>
2019-09-17 12:17:22 +0300

    feature: add buttons for sending and getting cookies on client

--- build/script.js
+++ build/script.js
@@ -1,1 +1,18 @@
-console.log('I am script');
+/* eslint-disable no-undef */
+
+const secoBtn = document.getElementById('seco');
+const gecoBtn = document.getElementById('geco');
+
+secoBtn.addEventListener('click', async () => {
+  const response = await fetch('/seco');
+  const data = await response.json();
+
+  console.log('data :', data);
+});
+
+gecoBtn.addEventListener('click', async () => {
+  const response = await fetch('/geco');
+  const data = await response.json();
+
+  console.log('data :', data);
+});
